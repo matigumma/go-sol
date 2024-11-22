a7bbf64163a6465f3485bbd256cd3ca08f526914
Mati <matiasgumma@gmail.com>
2024-11-22 20:02:09 -0300

    refactor: Simplify model initialization in main function

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	monitor := monitor.NewMonitor(tokenUpdates)
 	go monitor.Run()
 
-	model := ui.NewModel([]monitor.TokenInfo{})
+	model := ui.NewModel()
 
 	p := tea.NewProgram(model)
 
