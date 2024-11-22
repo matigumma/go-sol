381379491d298d2ba8237dfb6dfb00bf2552b584
Matias Gumma <matiasgumma@gmail.com>
2024-11-22 15:15:42 -0300

    feat: Enhance transaction logging and error handling in monitor module

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+
 	"github.com/fatih/color"
 
 	"github.com/gagliardetto/solana-go"
@@ -15,7 +16,6 @@ func Run() {
 	slog.Info(color.New(color.BgHiBlue).SprintFunc()("Connecting to WebSocket..."))
 	client, err := ConnectToWebSocket()
 	if err != nil {
-		slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Raw transaction data: %v", txRaw)))
 		slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Failed to connect to WebSocket: %v", err)))
 	}
 	defer client.Close()
@@ -59,40 +59,99 @@ func SubscribeToLogs(client *ws.Client, pubkey string) error {
 }
 
 func processLogMessage(msg *ws.LogResult) {
+	if msg.Value.Err != nil {
+		slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Transaction failed: %v", msg.Value.Err)))
+		return
+	}
+
 	signature := msg.Value.Signature
 	slog.Info(color.New(color.BgHiMagenta).SprintFunc()(fmt.Sprintf("Transaction Signature: %s", signature)))
 
 	rpcClient := rpc.New(rpc.MainNetBeta_RPC)
-	getTransactionDetails(rpcClient, signature.String())
+	getTransactionDetails(rpcClient, signature)
 }
 
-func getTransactionDetails(rpcClient *rpc.Client, signature string) {
+func getTransactionDetails(rpcClient *rpc.Client, signature solana.Signature) {
 	cero := uint64(0) // :/
 
-	slog.Info(color.New(color.BgHiBlue).SprintFunc()(fmt.Sprintf("Fetching transaction details for signature: %s", signature)))
-	txRaw, err := rpcClient.GetTransaction(
-		context.TODO(),
-		solana.MustSignatureFromBase58(signature),
-		&rpc.GetTransactionOpts{
-			Encoding:                       solana.EncodingJSONParsed,
-			Commitment:                     rpc.CommitmentConfirmed,
-			MaxSupportedTransactionVersion: &cero,
-		},
-	)
-	if err != nil {
-		slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Error fetching transaction: %v", err)))
-		slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Error fetching transaction: %v", err)))
-		return
-	}
+	{
+		// slog.Info(color.New(color.BgHiBlue).SprintFunc()("Fetching EncodingJSON transaction..."))
+		// txJson, err := rpcClient.GetTransaction(
+		// 	context.TODO(),
+		// 	signature,
+		// 	&rpc.GetTransactionOpts{
+		// 		Encoding:                       solana.EncodingJSONParsed,
+		// 		Commitment:                     rpc.CommitmentConfirmed,
+		// 		MaxSupportedTransactionVersion: &cero,
+		// 	},
+		// )
+		// if err != nil {
+		// 	slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Error fetching transaction: %v", err)))
+		// }
 
-	if tx.Meta != nil {
-		slog.Info(color.New(color.BgHiBlue).SprintFunc()(fmt.Sprintf("Transaction details: %+v", tx)))
-		for _, balance := range tx.Meta.PostTokenBalances {
-			if balance.Owner.String() == "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1" && balance.Mint.String() != "So11111111111111111111111111111111111111112" {
-				slog.Info(color.New(color.BgHiYellow).SprintFunc()("========== New Token Found =========="))
-				slog.Info(color.New(color.BgHiYellow).SprintFunc()(fmt.Sprintf("Mint Address: %s", balance.Mint)))
-				slog.Info(color.New(color.BgHiYellow).SprintFunc()("====================================="))
+		// if txJson != nil {
+		// 	spew.Dump(txJson)
+		// 	spew.Dump(txJson.Transaction.GetTransaction())
+		// }
+
+		// if txJson.Meta != nil {
+		// 	slog.Info(color.New(color.BgHiBlue).SprintFunc()(fmt.Sprintf("Transaction details: %+v", txJson)))
+		// 	for _, balance := range txJson.Meta.PostTokenBalances {
+		// 		if balance.Owner.String() == "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1" && balance.Mint.String() != "So11111111111111111111111111111111111111112" {
+		// 			slog.Info(color.New(color.BgHiYellow).SprintFunc()("========== New Token Found =========="))
+		// 			slog.Info(color.New(color.BgHiYellow).SprintFunc()(fmt.Sprintf("Mint Address: %s", balance.Mint)))
+		// 			slog.Info(color.New(color.BgHiYellow).SprintFunc()("====================================="))
+		// 		}
+		// 	}
+		// }
+	}
+	{
+		slog.Info(color.New(color.BgHiBlue).SprintFunc()("Fetching EncodingBase58 transaction..."))
+		tx58, err := rpcClient.GetTransaction(
+			context.TODO(),
+			signature,
+			&rpc.GetTransactionOpts{
+				Encoding:                       solana.EncodingBase58,
+				Commitment:                     rpc.CommitmentConfirmed,
+				MaxSupportedTransactionVersion: &cero,
+			},
+		)
+		if err != nil {
+			slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Error fetching EncodingBase58 transaction: %v", err)))
+		}
+
+		if tx58 != nil {
+			// spew.Dump(tx58)
+			// spew.Dump(tx58.Transaction.GetBinary())
+			// if txJson.Meta != nil {
+			for _, balance := range tx58.Meta.PostTokenBalances {
+				if balance.Owner.String() == "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1" && balance.Mint.String() != "So11111111111111111111111111111111111111112" {
+					slog.Info(color.New(color.BgHiYellow).SprintFunc()("========== New Token Found =========="))
+					slog.Info(color.New(color.BgHiYellow).SprintFunc()(fmt.Sprintf("Mint Address: %s", balance.Mint)))
+					slog.Info(color.New(color.BgHiYellow).SprintFunc()("====================================="))
+				}
 			}
 		}
 	}
+	// {
+	// 	slog.Info(color.New(color.BgHiBlue).SprintFunc()("Fetching EncodingBase64 transaction..."))
+	// 	tx64, err := rpcClient.GetTransaction(
+	// 		context.TODO(),
+	// 		signature,
+	// 		&rpc.GetTransactionOpts{
+	// 			Encoding:                       solana.EncodingBase64,
+	// 			Commitment:                     rpc.CommitmentConfirmed,
+	// 			MaxSupportedTransactionVersion: &cero,
+	// 		},
+	// 	)
+	// 	if err != nil {
+	// 		slog.Error(color.New(color.BgBlack, color.FgRed).SprintFunc()(fmt.Sprintf("Error fetching EncodingBase64 transaction: %v", err)))
+	// 	}
+
+	// 	if tx64 != nil {
+	// 		spew.Dump(tx64)
+	// 		spew.Dump(tx64.Transaction.GetBinary())
+	// 	}
+	// }
+
 }
